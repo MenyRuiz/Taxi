ab994b0069060fa43e70cf8a3b355a095b4cc176
Manuel Rincon <ruizjuan175@gmail.com>
2020-07-09 11:28:50 -0500

    Mejora del controlador

---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Taxi.Web.Data.Entities
+{
+
+    public class TaxiEntity
+    {
+        public int id { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string plaque { get; set; }
+    }
+}
