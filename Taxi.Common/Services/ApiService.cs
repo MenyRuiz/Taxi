b9886d6b6a985fa71769630033f332216c556247
Menyy Ruiz <ruizjuan175@gmail.com>
2020-07-15 17:51:15 -0500

    En linea Funcionando Correcatmente 20

---
+++ Taxi.Common/Services/ApiService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Taxi.Common.Models;
+
+namespace Taxi.Common.Services
+{
+    public class ApiService : IApiService
+    {
+        public Task<Response> GetTaxiAsync(string plaque, string urlBase, string servicePrefix, string controller)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Taxi.Web/Controllers/API/TaxisController.cs
+++ Taxi.Web/Controllers/API/TaxisController.cs
@@ -47,7 +47,9 @@ namespace Taxi.Web.Controllers.API
 
             if (taxiEntity == null)
             {
-                return NotFound();
+                taxiEntity = new TaxiEntity { plaque = plaque.ToUpper() };
+                _context.Taxis.Add(taxiEntity);
+                await _context.SaveChangesAsync();
             }
 
             return Ok(_converterHelper.ToTaxiResponse(taxiEntity));
