bdffbcf9e38942fab3294c5fe1412e15e2b2dbb2
Menyy Ruiz <ruizjuan175@gmail.com>
2020-07-15 18:41:41 -0500

    TaxiHistory 23

--- Taxi.Common/Services/ApiService.cs
+++ Taxi.Common/Services/ApiService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Taxi.Common.Models;
 
@@ -6,9 +8,43 @@ namespace Taxi.Common.Services
 {
     public class ApiService : IApiService
     {
-        public Task<Response> GetTaxiAsync(string plaque, string urlBase, string servicePrefix, string controller)
+        public async Task<Response> GetTaxiAsync(string plaque, string urlBase, string servicePrefix, string controller)
         {
-            throw new NotImplementedException();
+            try
+            {
+                HttpClient client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase),
+                };
+
+                string url = $"{servicePrefix}{controller}/{plaque}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                string result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                TaxiResponse model = JsonConvert.DeserializeObject<TaxiResponse>(result);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = model
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
         }
     }
 }
